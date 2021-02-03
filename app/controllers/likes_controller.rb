baa5ada88506b2c42219cb31fa1ca57fab88dff7
github username <github email address>
2021-02-03 01:29:11 -0500

    created index controller action for likes controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,10 @@
 class LikesController < ApplicationController
+
+    def index
+        likes = Like.all 
+        render json: likes, include: [:dishes, :cuisines, :comments] 
+    end 
+
+
+
 end
