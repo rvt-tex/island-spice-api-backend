d2fc05b1758482877dfc498633fcbf9b7bebae38
github username <github email address>
2021-02-03 02:32:25 -0500

    created controller actions, create, destroy and dish params for dish controller

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -6,4 +6,32 @@ class DishesController < ApplicationController
         render json: dishes, include: [:likes, :cuisine, :comments]
     end 
 
+    def create 
+        dish = Dish.new(dish_params)
+        if dish.save 
+            render json: DishSerializer.new(dish)
+        else 
+            render json: {errors: pin.errors.full_messages}, status: :unprocessible_entity
+        end 
+    end 
+
+    def destroy 
+        dish = Dish.find_by_id(params[:id])
+        if dish.destroy
+            Dishes = Dish.all 
+            render json: DishSerializer.new(dishes)
+        else
+            render json: { message: "error"}  
+        end 
+    end 
+
+    private
+
+    def dish_params
+        params.require(:dish).permit(:title, :description, :img_url, :cuisine_id, :like_id, :comment_id)
+    end 
+
+    def find_dish
+        @dish = Dish.find_by_id(params[:id])
+    end
 end
