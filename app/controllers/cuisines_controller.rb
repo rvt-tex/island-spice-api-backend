8d4d21532bbbbc606f47452b2cdaa819aa1a0338
github username <github email address>
2021-02-03 01:32:24 -0500

    created index action for cuisines controller

--- app/controllers/cuisines_controller.rb
+++ app/controllers/cuisines_controller.rb
@@ -1,2 +1,8 @@
 class CuisinesController < ApplicationController
+
+    def index
+        cuisines = Cuisine.all 
+        render json: cuisines, include: [:dishes, :likes, :comments] 
+    end 
+
 end
